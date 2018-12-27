841f8adfce376179c0ba68304c971ed677d682f7
Sujal Mandal <smandal@operative.com>
2018-12-27 14:14:41 +0530

    added examples for maps

---
+++ main/maps.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//maps in go start with a special keyword "map" followed by the type of index inside square braces
+	//and the type of key e.g. myMap:=map[string]string
+	var empContactDb map[string]string
+	empContactDb = make(map[string]string)
+	//put values inside the map
+	empContactDb["sujal"] = "8904711202"
+	empContactDb["david"] = "9943103892"
+	empContactDb["david"] = "8904711202"
+	//access value inside the map by key
+	fmt.Println(empContactDb["sujal"])
+}
